8b87136fde069b0bb7844fc1096b0baa0348ca9b
97fangz <briansegem25@gmail.com>
2023-01-14 04:18:35 -0800

    4-hiddden_discovery.py

--- 0x02-python-import_modules/4-hiddden_discovery.py
+++ 0x02-python-import_modules/4-hiddden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from hidden_4 import *
     arr = dir()
-    for i in range(len(arr)):
-        if (arr[i][:2] != "__"):
-            print("{}".format(arr[i]))
+    for a in range(0, len(arr)):
+        if (arr[a][0:2] != "__"):
+            print("{}".format(arr[a]))
 
