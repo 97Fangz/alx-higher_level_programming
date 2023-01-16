2d781e296404692707fb5c2398a68c0a5e12324b
97fangz <briansegem25@gmail.com>
2023-01-16 05:42:30 -0800

    python_classes

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""class Square"""
+
+
+class Square:
+    """Represents a square
+    Attribute:
+    _size (int): size of a side of the square
+    """
+    def __init__(self, size=0):
+        """initializes the square
