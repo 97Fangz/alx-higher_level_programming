c2ed576572421f1b281994557f2a377a80995315
97fangz <briansegem25@gmail.com>
2023-01-23 03:25:18 -0800

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""Locked Class"""
+
+
 class LockedClass:
+    """ locked slots"""
     __slots__ = ['first_name']
-    def __init__(self, first_name=''):
-        self.first_name = first_name
