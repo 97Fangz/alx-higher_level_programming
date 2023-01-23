bc1fc70572d3222ae4f58e55395d72fd09955224
97fangz <briansegem25@gmail.com>
2023-01-23 00:39:30 -0800

    8-rectangle.py

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -106,10 +106,11 @@ class Rectangle:
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Finds the biggest Rectangle based on the area
-        
+
         Args:
             rect_1: Rectangle instance
             rect_2: Rectangle instance different from rect_1
+
         Returns:
             The instance with the biggest area,
             or rect_1 if both rectangles have the same area
