0aab3fe5f47d7ed874ee7530e3d081650d7ee86c
97Fangz <briansegem25@gmail.com>
2023-01-23 11:52:57 +0300

    "\r\n        " ""

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -75,7 +75,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Sets the height of a Rectangle instance
-        
+
         Args:
             value: value of the height, must be a positive integer
         """
@@ -87,6 +87,7 @@ class Rectangle:
 
     def area(self):
         """Calculates the area of a Rectangle instance
+        
         Returns:
             Area of the the rectangle, given by height * width
         """
