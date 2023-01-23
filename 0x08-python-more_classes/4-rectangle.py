2b8c081867f72c1bf7d7e4b24adb11d85c59fc4a
97Fangz <briansegem25@gmail.com>
2023-01-23 10:34:12 +0300

    ""

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -2,3 +2,8 @@
 """Module 4-rectangle
 Defines a Rectangle class.
 """
+class Rectangle:
+    """Rectangle class defined by width and height."""
+
+    def __init__(self, width=0, height=0):
+        """Initializes a Rectan
