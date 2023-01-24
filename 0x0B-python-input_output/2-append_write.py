262ec67e76ed3db73e0a86ab6cdc3b0f116264cb
97Fangz <briansegem25@gmail.com>
2023-01-24 09:00:12 +0300

    "\r\n    "

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module append_write. Contains the function append_write
+"""
+
+
+def append_write(filename="", text=""):
+    """Appends text to filename.
+    
+    Args:
+        filename: name of the file
+        text: text to append
+    Return: the number of characters added
+    """
+
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
