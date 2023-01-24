485e5c2749aa1b1d18f1a568cd928b9643b15563
97Fangz <briansegem25@gmail.com>
2023-01-24 08:58:43 +0300

    "\r\n    "

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module for the read_file function
+"""
+
+
+def read_file(filename=""):
+    """Reads from filename and prints it's contents to stdout.
+    
+    Args:
+        filename: name of the file
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
