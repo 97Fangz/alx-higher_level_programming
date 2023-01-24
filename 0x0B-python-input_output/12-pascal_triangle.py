c5ec52cbf7bb8349d14c54d3d98a267afaddb1a9
97fangz <briansegem25@gmail.com>
2023-01-24 02:55:47 -0800

    12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -11,14 +11,14 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    l = [[0 for a in range(i + 1)] for i in range(n)]
-    l[0] = [1]
+    a = [[0 for a in range(i + 1)] for i in range(n)]
+    a[0] = [1]
 
     for i in range(1, n):
-        l[i][0] = 1
+        a[i][0] = 1
         for j in range(1, i + 1):
-            if j < len(l[i - 1]):
-                l[i][j] = l[i - 1][j - 1] + l[i - 1][j]
+            if j < len(a[i - 1]):
+                a[i][j] = a[i - 1][j - 1] + a[i - 1][j]
             else:
-                l[i][j] = l[i - 1][0]
-    return l
+                a[i][j] = a[i - 1][0]
+    return a
