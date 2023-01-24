8b9a0f008f3a9477b9ecc57a354aef0bbf4c6f16
97fangz <briansegem25@gmail.com>
2023-01-23 21:15:57 -0800

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Module 0-lookup. Finding a list of available attributes & methods of an object.
+"""Module 0-lookup. Finding a list of available attributes &
+methods of an object.
 """
 
 
