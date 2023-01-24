986b9600c4b9f51047debd85e97ed5d6f3b369d4
97Fangz <briansegem25@gmail.com>
2023-01-24 08:59:43 +0300

    "\r\n    "

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module write_file. Contains the function write_file
+"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file and returns the number of characters
+    written.
+    
+    Args:
+        filename: name of the file
+        text: string to write in the file
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
