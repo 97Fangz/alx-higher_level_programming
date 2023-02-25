7ee8a68636010035a7d8d3668b2035168c375baa
97fangz <briansegem25@gmail.com>
2023-02-25 01:28:05 -0800

    16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,3 @@
--- lists the number of records with the same score in the table second_table
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
+-- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
+-- lists all records in a table except rows without a name value
+SELECT `score`, `name` FROM second_table WHERE `name` IS NOT NULL ORDER BY `score` DESC;
