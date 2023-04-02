a6dfa21dbd44ae97c3a1388eac1e94dde6fdfb3d
97fangz <briansegem25@gmail.com>
2023-04-02 04:50:17 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,19 +5,12 @@ Module for fetching the status of https://alx-intranet.hbtn.io/status
 
 import urllib.request
 
+url = 'https://alx-intranet.hbtn.io/status'
 
-def get_status():
-    """
-    Fetches the status of https://alx-intranet.hbtn.io/status
-    and returns the body of the response as a bytes object
-    """
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        return response.read()
+with urllib.request.urlopen(url) as response:
+    html = response.read()
 
-if __name__ == "__main__":
-    # Get the status and print the body of the response
-    status = get_status()
-    print("Body response:")
-    print("\t- type:", type(status))
-    print("\t- content:", status)
-    print("\t- utf8 content:", status.decode('utf-8'))
+print("- type: {}".format(type(html)))
+print("- content: {}".format(html))
+print("- utf8 content: {}".format(html.decode('utf-8')))
+
